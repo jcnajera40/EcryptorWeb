1dfa531364b0b4f287b4a5b3efca496bed526da6
jcnajera40 <jcnajera@live.com>
2018-04-10 22:24:04 -0600

    Add project files.

---
+++ EcryptorWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EcryptorWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
